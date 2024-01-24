44ca793bf666af610cafc10d341afba7fa0d78c1
pappinm <marin@pappin.ca>
2024-01-24 15:51:56 -0500

    finished task 4b, added oilslip and barrels that you can push

---
+++ Assets/Week 3/Scripts/Oil.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oil : MonoBehaviour
+{
+    float drag;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        drag = collision.attachedRigidbody.angularDrag;
+        collision.attachedRigidbody.angularDrag =0;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        collision.attachedRigidbody.angularDrag = drag;
+    }
+}
